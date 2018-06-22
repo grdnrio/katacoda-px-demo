e68b8d17884d02a3bb692729bd5761d5e4bd0516
Joe Gardiner <joe@grdnr.io>
2018-06-22 16:46:57 +0100

    Added lighthouse deployment

---
+++ px-demo-k8s/assets/courseData.sh
@@ -0,0 +1,5 @@
+sudo docker run --restart=always                            \
+       --name px-lighthouse -d                              \
+       -p 80:80 -p 443:443                                  \
+       -v /etc/pwxlh:/config -v /etc/pwxlh/certs:/certs     \
+       portworx/px-lighthouse:1.4.0-rc1
