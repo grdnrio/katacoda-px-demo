d8cc4b9ebd12d739fea33cb02b3a5cb55f374915
Joe Gardiner <joe@grdnr.io>
2018-06-22 16:53:09 +0100

    moved to background script

--- px-demo-k8s/background.sh
+++ px-demo-k8s/background.sh
@@ -1,1 +1,7 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
+
+sudo docker run --restart=always                            \
+       --name px-lighthouse -d                              \
+       -p 80:80 -p 443:443                                  \
+       -v /etc/pwxlh:/config -v /etc/pwxlh/certs:/certs     \
+       portworx/px-lighthouse:1.4.0-rc1
--- px-demo-k8s/courseData.sh
+++
@@ -1,5 +0,0 @@
-sudo docker run --restart=always                            \
-       --name px-lighthouse -d                              \
-       -p 80:80 -p 443:443                                  \
-       -v /etc/pwxlh:/config -v /etc/pwxlh/certs:/certs     \
-       portworx/px-lighthouse:1.4.0-rc1
