a384a3ab94961b56a460b59939f825a05ec5d487
Joe Gardiner <joe@grdnr.io>
2018-07-03 11:12:03 +0100

    Added lighthouse startup

--- px-demo-k8s/foreground.sh
+++ px-demo-k8s/foreground.sh
@@ -1,7 +1,8 @@
-launch.sh
-
 sudo docker run --restart=always                            \
        --name px-lighthouse -d                              \
        -p 80:80 -p 443:443                                  \
        -v /etc/pwxlh:/config -v /etc/pwxlh/certs:/certs     \
        portworx/px-lighthouse:1.4.0-rc1
+
+launch.sh
+
