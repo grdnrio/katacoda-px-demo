4d3eac0a4e3f6c03275ff783424a7e0536e5a53d
Joe Gardiner <joe@grdnr.io>
2018-07-05 07:32:49 +0100

    switched to rc1 for 1.3 compatibility

--- px-demo-k8s/foreground.sh
+++ px-demo-k8s/foreground.sh
@@ -2,7 +2,7 @@ sudo docker run --restart=always                            \
        --name px-lighthouse -d                              \
        -p 80:80 -p 443:443                                  \
        -v /etc/pwxlh:/config -v /etc/pwxlh/certs:/certs     \
-       portworx/px-lighthouse:1.4.0-rc2
+       portworx/px-lighthouse:1.4.0-rc1
 
 mkdir wordpress && mkdir postgres
 mv wordpress*.yaml wordpress && mv wordpress*.sh wordpress
