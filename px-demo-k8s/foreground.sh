7cfa713d8d2392c783b09e0a522f08c5955d67c2
Joe Gardiner <joe@grdnr.io>
2018-07-11 21:13:54 +0100

    add px-spec for manual install

--- px-demo-k8s/foreground.sh
+++ px-demo-k8s/foreground.sh
@@ -11,6 +11,8 @@ mv postgres*.* postgres
 
 echo -e "\n alias k='kubectl' \nalias ls='ls -l --color=auto' \nPS1='\h:\w\\$ '" >> .bashrc && source ~/.profile
 
+curl -L -s -o px-spec.yaml "http://install.portworx.com/1.4/?c=px-demo-14f79fe5-ecbc-4271-af4d-36c25c9c98c8&k=etcd://master:4001&kbver=1.10.0&s=/dev/vdb&st=k8s&stork=true"
+
 clear
 
 launch.sh
